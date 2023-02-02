78048c91b72e5f211116502a6087aae3a446222b
samueller9 <60405236+samueller9@users.noreply.github.com>
2023-02-01 16:33:58 -0800

    Initial Commit

---
+++ Magic_Eight_Ball_3.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic_Eight_Ball_3.0
+//
+//  Created by Stefan Mueller on 2/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
